8203886fa50f777768bedb78da51e99474c89799
SaiHo Ip <saiho.ip@mindlayer.io>
2025-05-03 22:44:41 +0800

    Add Azure SDK integration and update environment variables; remove index.js

--- ai-sdk-1/README.md
+++ ai-sdk-1/README.md
@@ -1,4 +1,6 @@
-### Start Playwright MCP locally
+## Connecting an MCP Server using the Vercel AI SDK
+
+### Running a Local PlayWright MCP Server Instance
 
 ```
 npx @playwright/mcp@latest --port 8931
--- ai-sdk-1/index.ts
+++ ai-sdk-1/index.ts
@@ -1,5 +1,7 @@
+import { createAzure } from "@ai-sdk/azure";
 import { createOpenRouter } from "@openrouter/ai-sdk-provider";
-import { experimental_createMCPClient, generateText } from "ai";
+import { experimental_createMCPClient, generateText, tool } from "ai";
+import { z } from "zod";
 
 import "dotenv/config";
 
@@ -17,13 +19,22 @@ const main = async () => {
 
   const tools = await client.tools();
 
-  const response = await generateText({
-    model: openrouter.chat("google/gemma-3-27b-it:free"),
-    tools,
-    prompt: "Open hk.yahoo.com and get the latest news",
+  const azure = createAzure({
+    resourceName: "poc-test-embedding",
+    apiKey: process.env.AZURE_GTP4O_API_KEY,
+    apiVersion: "2024-12-01-preview",
   });
 
-  console.log(JSON.stringify(response, null, 2));
+  const llm = azure("gpt-4o");
+
+  const response = await generateText({
+    model: llm,
+    prompt: "Open hk.yahoo.com and get the top one financial news",
+    tools,
+  });
+  const { steps, text, toolResults } = response;
+
+  console.log(text);
 };
 
 main();
